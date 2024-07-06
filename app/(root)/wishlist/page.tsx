3f82488bf75ff56aba634ad7ecbc3d00172852a6
pankajjsdev <pankaj.js.dev@gmail.com>
2024-07-06 11:17:11 +0530

    complete cart & Wishlist page ui

--- app/(root)/wishlist/page.tsx
+++ app/(root)/wishlist/page.tsx
@@ -1,5 +1,16 @@
+import QuantityButton from "@/components/_ui/cart/QuantityButton"
 import { Button } from "@/components/ui/button"
-import { BsX } from "react-icons/bs";
+
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+} from "@/components/ui/card"
+import { IoMdCloseCircleOutline } from "react-icons/io";
+
 import {
     Table,
     TableBody,
@@ -11,46 +22,66 @@ import {
 } from "@/components/ui/table"
 import Image from "next/image"
 
+
 function page() {
     return (
-        <div className="container my-3">
-            <h1 className="text-[32px] text-center my-10">My Wishlist</h1>
-            <Table className="rounded-lg border border-gray-gray1  my-10">
-                {/* <TableCaption>A list of your recent invoices.</TableCaption> */}
-                <TableHeader>
-                    <TableRow className="border-gray-gray1">
-                        <TableHead className="">Product</TableHead>
-                        <TableHead>price</TableHead>
-                        <TableHead>Stock Status</TableHead>
-                        <TableHead className="text-right"></TableHead>
-                    </TableRow>
-                </TableHeader>
-                <TableBody>
-                   {
-                    [1,2,3,4,5]?.map((item,index)=>{
-                        return(
-                            <TableRow key={index} className="border-gray-gray1">
-                            <TableCell className="font-medium flex items-center justify-center">
-                                <Image
-                                    src={"/images/products/product1.png"}
-                                    width={80}
-                                    height={80}
-                                    alt=""
-                                />
-                                <h1>    Green Capsicum</h1>
-                            </TableCell>
-                            <TableCell>$14.99</TableCell>
-                            <TableCell><span className={`p-1 text-white ${true ? 'bg-primary-primary' : 'bg-secondary-danger'}`}>In Stock</span></TableCell>
-                            <TableCell className="flex">
-                                <Button>add to cart</Button>
-                                <BsX size={30}/>
-                            </TableCell>
-                        </TableRow>
-                        )
-                    })
-                   }
-                </TableBody>
-            </Table>
+        <div className="container my-8">
+            <h1 className="text-center font-semibold text-3xl py-6">My Wishlist</h1>
+            <section className="">
+                <div className="">
+                    <div className="rounded-md border border-gray-gray1 shadow">
+                        <Table>
+                            {/* <TableCaption>A list of your recent invoices.</TableCaption> */}
+                            <TableHeader>
+                                <TableRow>
+                                    <TableHead className="">Product</TableHead>
+                                    <TableHead>price</TableHead>
+                                    <TableHead>Stock Status</TableHead>
+                                    <TableHead></TableHead>
+                                </TableRow>
+                            </TableHeader>
+                            <TableBody>
+                                {
+                                    [1, 2, 3]?.map((item, index) => {
+                                        return (
+                                            <TableRow key={index}>
+                                                <TableCell className="font-medium flex items-center">
+                                                    <Image
+                                                        src={"/images/products/product1.png"}
+                                                        width={80}
+                                                        height={80}
+                                                        alt=""
+                                                    />
+                                                    <h1>    Green Capsicum</h1>
+                                                </TableCell>
+                                                <TableCell>$14.00</TableCell>
+
+                                                <TableCell ><span className="px-2 py-1 bg-primary-soft text-primary-hard rounded text-[14px]"> In stack </span></TableCell>
+                                                <TableCell>
+                                                    <div className="flex items-center justify-around">
+                                                        <Button className="bg-primary-primary text-white rounded-full  text-[16px] font-semibold">
+                                                            Add to Cart</Button>
+                                                        <IoMdCloseCircleOutline className="text-gray-gray5 hover:text-secondary-danger cursor-pointer" size={25} />
+
+                                                    </div>
+                                                </TableCell>
+                                            </TableRow>
+                                        )
+                                    })
+                                }
+                            </TableBody>
+                        </Table>
+                        <div className="flex items-center justify-between border-t border-gray-gray1 py-3 px-3">
+                            <div className="font-medium flex items-center">
+                                <span className="mx-2">Share: </span>
+                                <Button className="bg-gray-gray.5 rounded-full text-gray-gray7 text-[14px] font-semibold mx-2">F</Button>
+                                <Button className="bg-gray-gray.5 rounded-full text-gray-gray7 text-[14px] font-semibold">I</Button>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </section>
         </div>
     )
 }
