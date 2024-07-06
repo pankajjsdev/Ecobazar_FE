97c73d94daa65740a4c20aae4090ca233e1a332e
pankajjsdev <pankaj.js.dev@gmail.com>
2024-07-06 11:02:47 +0530

    work on cart page

--- app/(root)/cart/page.tsx
+++ app/(root)/cart/page.tsx
@@ -9,7 +9,8 @@ import {
     CardHeader,
     CardTitle,
 } from "@/components/ui/card"
-
+import { Input } from "@/components/ui/input"
+import { IoMdCloseCircleOutline } from "react-icons/io";
 
 import {
     Table,
@@ -26,6 +27,7 @@ import Image from "next/image"
 function page() {
     return (
         <div className="container py-6">
+            <h1 className="text-center font-semibold text-3xl py-6">My Shopping Cart</h1>
             <section className="grid grid-cols-4 gap-3">
                 <div className="col-span-3">
                     <div className="rounded-md border border-gray-gray1 shadow">
@@ -37,6 +39,7 @@ function page() {
                                     <TableHead>price</TableHead>
                                     <TableHead>Quantity</TableHead>
                                     <TableHead className="">Subtotal</TableHead>
+                                    <TableHead></TableHead>
                                 </TableRow>
                             </TableHeader>
                             <TableBody>
@@ -52,13 +55,50 @@ function page() {
                                     </TableCell>
                                     <TableCell>$14.00</TableCell>
                                     <TableCell>
-                                        <QuantityButton/>
+                                        <QuantityButton />
                                     </TableCell>
                                     <TableCell className="text-right">$250.00</TableCell>
+                                    <TableCell><IoMdCloseCircleOutline className="text-gray-gray5 hover:text-secondary-danger cursor-pointer" size={25}/></TableCell>
+                                </TableRow>
+
+                                <TableRow>
+                                    <TableCell className="font-medium flex items-center">
+                                        <Image
+                                            src={"/images/products/product1.png"}
+                                            width={80}
+                                            height={80}
+                                            alt=""
+                                        />
+                                        <h1>    Green Capsicum</h1>
+                                    </TableCell>
+                                    <TableCell>$14.00</TableCell>
+                                    <TableCell>
+                                        <QuantityButton />
+                                    </TableCell>
+                                    <TableCell className="text-right">$250.00</TableCell>
+                                    <TableCell><IoMdCloseCircleOutline className="text-gray-gray5 hover:text-secondary-danger cursor-pointer" size={25}/></TableCell>
                                 </TableRow>
                             </TableBody>
                         </Table>
+                        <div className="flex items-center justify-between border-t border-gray-gray1 py-3 px-3">
+                            <div className="font-medium flex items-center">
+                                <Button className="bg-gray-gray.5 rounded-full text-gray-gray7 text-[14px] font-semibold">Return to shop</Button>
+                            </div>
 
+
+                            <div className="text-right">
+                                <Button className="bg-gray-gray.5 rounded-full text-gray-gray7 text-[14px] font-semibold">Update Cart</Button>
+                            </div>
+                        </div>
+
+
+                    </div>
+                    <div className="rounded-lg  border border-gray-gray1 my-6 py-6 px-3 flex items-center justify-around "> 
+                      <p className="px-3 text-xl">Coupon Code</p>
+                        <div className="flex items-center w-1/2 relative ">
+                            <input className="w-full h-12 border border-r-0 px-2  border-gray-gray1 focus:border-gray-gray1 rounded-full rounded-r-none" />
+                            <Button className="bg-gray-gray9 rounded-full text-white text-[14px] py-6 font-semibold px-6 absolute left-[80%]">Apply Coupon</Button>
+                        </div>
                     </div>
                 </div>
                 <div className="">
@@ -79,7 +119,7 @@ function page() {
                                     <h3 className="text-lg font-semibold">$84.00</h3>
                                 </div>
                                 <div>
-                                    <Button className="bg-primary-primary text-white rounded-full w-full">
+                                    <Button className="bg-primary-primary text-white rounded-full w-full text-[16px] font-semibold">
                                         Proceed to checkout</Button>
                                 </div>
                             </div>
